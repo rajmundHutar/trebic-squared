1aae62c37666d6f83f629342c1d20ac57176e4a7
Raja Hutar <rajmund.hutar@gmail.com>
2020-04-05 23:37:43 +0200

    - versioning css and js - Zoom and Drag - even on mobile

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -11,7 +11,9 @@ abstract class BasePresenter extends Presenter {
 	protected function startup() {
 		parent::startup();
 
-		$this->template->version = md5_file(__DIR__ . '/../../www/css/main.css');
+		$css = md5_file(__DIR__ . '/../../www/css/main.css');
+		$js = md5_file(__DIR__ . '/../../www/js/main.js');
+		$this->template->version = md5($css . $js);
 
 	}
 
--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,109 @@
+let isDragging = false;
+let wasMoved = false;
+let zoom = 1;
+let start = {};
+let beginPosition = {};
+let newPosition = {};
+let game;
+
+function repeatOften() {
+	if (game) {
+		game.style.left = newPosition.x + 'px';
+		game.style.top = newPosition.y + 'px';
+	}
+	requestAnimationFrame(repeatOften);
+}
+requestAnimationFrame(repeatOften);
+
+function down(e) {
+
+	start.x = e.clientX || e.touches[0].clientX;
+	start.y = e.clientY || e.touches[0].clientY;
+
+	isDragging = true;
+	wasMoved = false;
+
+	beginPosition.x = parseInt(game.style.left);
+	beginPosition.y = parseInt(game.style.top);
+
+	if (!e.touches){
+		e.preventDefault();
+	}
+
+}
+
+function up(e) {
+
+	isDragging = false;
+
+	let x = e.clientX || e.changedTouches[0].clientX
+	let y = e.clientY || e.changedTouches[0].clientY
+
+	if (!e.touches && Math.abs(x - start.x) > 5 || Math.abs(y - start.y) > 5) {
+		wasMoved = true;
+	}
+
+}
+
+function leave(e) {
+	isDragging = false;
+	e.preventDefault();
+}
+
+function move(e) {
+	if (isDragging) {
+		let x = e.clientX || e.changedTouches[0].clientX;
+		let newX = beginPosition.x + x - start.x;
+		newX = Math.min(newX, 0);
+		newX = Math.max(newX, -(zoom - 1) * document.documentElement.clientWidth);
+
+		let y = e.clientY || e.changedTouches[0].clientY;
+		let newY = beginPosition.y + y - start.y;
+		newY = Math.min(newY, 0);
+		newY = Math.max(newY, -(zoom - 1) * 0.64 * document.documentElement.clientWidth);
+
+		newPosition.x = newX;
+		newPosition.y = newY;
+	}
+
+}
+
 window.addEventListener('load', (e) => {
+
+	game = document.getElementById('game');
+
+	game.style.top = '0px';
+	game.style.left = '0px';
+
+	game.addEventListener('mousedown', down);
+	game.addEventListener('touchstart', down);
+
+	game.addEventListener('mouseup', up);
+	game.addEventListener('touchend', up);
+
+	game.addEventListener('mouseleave', leave);
+	game.addEventListener('touchcancel', leave);
+
+	game.addEventListener('mousemove', move);
+	game.addEventListener('touchmove', move);
+
+	document.querySelectorAll('.zoom').forEach((zoomElement) => {
+		zoomElement.addEventListener("click", (e) => {
+			zoom = parseInt(e.target.dataset.zoom);
+			game.style.width = (100 * zoom) + '%';
+			game.style.height = (63 * zoom) + 'vw';
+			newPosition.x = 0;
+			newPosition.y = 0;
+		});
+	});
 
 	document.querySelectorAll('.square').forEach((square) => {
 		square.addEventListener("click", (e) => {
+
+			if (wasMoved) {
+				return;
+			}
+
 			var coord = e.target.dataset.coordinates;
 
 			var guessElement = document.getElementById('guess');
@@ -69,6 +171,9 @@ window.addEventListener('load', (e) => {
 });
 
 function toggleModal(id) {
-	document.getElementById(id).classList.toggle('is-active');
-	document.getElementById('html').classList.toggle('is-clipped');
+	let ele = document.getElementById(id);
+	if (ele) {
+		ele.classList.toggle('is-active');
+		document.getElementById('html').classList.toggle('is-clipped');
+	}
 }
